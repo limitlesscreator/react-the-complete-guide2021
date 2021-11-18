8d926d9dc121f61cc3d9874c00ea25d92b71c262
Vladislav V <0428vlad0428@mail.ru>
2021-11-18 16:16:46 +0300

    finished 42

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import {ExpenseItem} from "./components/ExpenseItem";
+import {ExpenseItem} from "./components/Expenses/ExpenseItem";
+import {Expenses} from "./components/Expenses/Expenses";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    const expenses = [
+        {
+            id: 'el1',
+            title: 'Car Insurance',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {
+            id: 'el2',
+            title: 'New TV',
+            amount: 799.49,
+            date: new Date(2020, 2, 12),
+        },
+    ]
+    return (
+        <div className="App">
+            <h2>Let's get started!</h2>
+            <div>
+                <Expenses expenses={expenses}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
